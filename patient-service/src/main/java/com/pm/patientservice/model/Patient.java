ba9ff2ae99109aefc9671a9d4597b36ec89a29bf
Patryk Kubicki <patryk.kubicki1th@gmail.com>
2026-04-28 14:01:34 +0200

    Add getPatients endpoint

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -30,7 +30,7 @@ public class Patient {
     private LocalDate dateOfBirth;
 
     @NotNull
-    private LocalDate registerDate;
+    private LocalDate registeredDate;
 
     public UUID getId() {
         return id;
@@ -72,11 +72,11 @@ public class Patient {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public LocalDate getRegisterDate() {
-        return registerDate;
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
     }
 
-    public void setRegisterDate(LocalDate registerDate) {
-        this.registerDate = registerDate;
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
     }
 }
